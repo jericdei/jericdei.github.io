7cf53f709bfd678385fddb0035efe4e58ea7d13d
Jeric June Logan <loganjeric25@gmail.com>
2024-04-13 20:27:11 +0800

    updates

--- src/components/vue/Menu.vue
+++ src/components/vue/Menu.vue
@@ -1,31 +1,50 @@
 <script setup lang="ts">
-import { ref, Transition } from 'vue';
-import MenuListItem from './MenuListItem.vue';
+import { ref, Transition } from 'vue'
+import MenuListItem from './MenuListItem.vue'
 
 const menuVisible = ref(false)
+
+const props = defineProps<{
+    items: {
+        label: string
+        path: string
+    }[]
+}>()
 </script>
 
 <template>
-	<Transition>
-		<div>
-			<button @click="menuVisible = !menuVisible">
-				<i class="ri-xl transition-all" :class="menuVisible ? 'ri-close-line' : 'ri-menu-line'"></i>
-			</button>
+    <Transition>
+        <div>
+            <button @click="menuVisible = !menuVisible">
+                <i
+                    class="ri-xl transition-all"
+                    :class="menuVisible ? 'ri-close-line' : 'ri-menu-line'"
+                ></i>
+            </button>
 
-			<ul class="absolute flex flex-col gap-4 left-0 text-slate-900 w-full text-center bg-gray-400 my-4 py-4 rounded-xl" :class="{ hidden: !menuVisible }">
-				<MenuListItem label="Home" href="/" />
-				<MenuListItem label="Blog" href="/blog" />
-				<MenuListItem label="Contact" href="/contact" />
-			</ul>
-		</div>
-	</Transition>
+            <nav
+                class="absolute text-slate-900 text-center bg-gray-400 rounded-xl top-48 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[30rem]"
+            >
+                <ul class="flex flex-col gap-4 p-4" :class="{ hidden: !menuVisible }">
+                    <MenuListItem
+                        v-for="item in props.items"
+                        :key="item.path"
+                        :label="item.label"
+                        :href="item.path"
+                    />
+                </ul>
+            </nav>
+        </div>
+    </Transition>
 </template>
 
 <style scoped>
-.v-enter-active, .v-leave-active {
-  transition: opacity 0.8s;
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.8s;
 }
-.v-enter, .v-leave-to {
-  opacity: 0;
+.v-enter,
+.v-leave-to {
+    opacity: 0;
 }
 </style>
--- src/components/vue/MenuListItem.vue
+++ src/components/vue/MenuListItem.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 const props = defineProps<{
-	label?: string
+    label?: string
 }>()
 </script>
 
 <template>
-	<a class="w-full hover:bg-gray-600 hover:text-slate-200 py-2 cursor-pointer transition-all" v-bind="$attrs">
-		<slot>
-			<span>{{ props.label }}</span>
-		</slot>
-	</a>
+    <a
+        class="w-full hover:bg-gray-600 hover:text-slate-200 py-2 cursor-pointer transition-all rounded-xl"
+        v-bind="$attrs"
+    >
+        <slot>
+            <span>{{ props.label }}</span>
+        </slot>
+    </a>
 </template>
