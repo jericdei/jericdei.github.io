cd0c35d379a86253cea3466a25fa0a4483e29821
Jeric June Logan <loganjeric25@gmail.com>
2024-05-09 21:21:49 +0800

    rename types

---
+++ src/components/vue/TechStackList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { CollectionEntry } from 'astro:content'
+import Tag from 'primevue/tag'
+
+const props = defineProps<{
+    technologies: CollectionEntry<'technologies'>[]
+}>()
+
+const types = Array.from(
+    new Set(props.technologies.flatMap((technology) => technology.data.type)),
+)
+</script>
+
+<template>
+    <div>
+        <div class="flex flex-wrap justify-center gap-2">
+            <Tag
+                v-for="itm in types"
+                :key="itm"
+                :value="itm"
+                :pt="{ value: 'text-lg' }"
+            />
+        </div>
+    </div>
+</template>
