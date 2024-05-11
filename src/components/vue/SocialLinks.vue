d8af3e15f7dbb0c430e96ea4d118c8fa9018a215
Jeric June Logan <loganjeric25@gmail.com>
2024-05-11 22:51:05 +0800

    resume

--- src/components/vue/SocialLinks.vue
+++ src/components/vue/SocialLinks.vue
@@ -7,7 +7,11 @@ import Button from 'primevue/button'
         class="mt-12 flex flex-wrap items-center justify-center gap-2 lg:justify-start"
     >
         <a v-tooltip.top="'Hit me up'" href="mailto:loganjeric25@gmail.com">
-            <Button client:load icon="ri-mail-line ri-lg" />
+            <Button icon="ri-mail-line ri-lg" />
+        </a>
+
+        <a v-tooltip.top="'View my Resume'" href="/resume.pdf" target="_blank">
+            <Button icon="ri-file-pdf-2-fill ri-lg" severity="danger" />
         </a>
 
         <a
@@ -15,11 +19,7 @@ import Button from 'primevue/button'
             href="https://github.com/jericdei"
             target="_blank"
         >
-            <Button
-                client:load
-                severity="secondary"
-                icon="ri-github-fill ri-lg"
-            />
+            <Button severity="secondary" icon="ri-github-fill ri-lg" />
         </a>
 
         <a
@@ -28,7 +28,6 @@ import Button from 'primevue/button'
             target="_blank"
         >
             <Button
-                client:load
                 class="!border-[#0077b5] !bg-[#0077b5] !text-white !outline-[#0077b5] hover:!bg-[#0077b5] hover:!outline-[#0077b5]"
                 icon="ri-linkedin-fill ri-lg"
             />
