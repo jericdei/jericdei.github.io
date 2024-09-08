bf0b6d4b2a7df20cb2fb8e5ddcabcea938da331a
Jeric June Logan <loganjeric25@gmail.com>
2024-09-08 11:39:29 +0800

    wip: career timeline

---
+++ src/components/vue/CareerTimeline.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Timeline from "primevue/timeline"
+import { ref } from "vue";
+
+type Event = {
+  date: Date
+  position: string
+  company: string
+}
+
+const events = ref<Event[]>([
+  {
+    date: new Date('2022-08-23'),
+    position: 'Web Developer',
+    company: 'Prime Outsourcing'
+  },
+  {
+    date: new Date('2023-01-16'),
+    position: 'Junior Full Stack Developer',
+    company: 'Lhoopa'
+  },
+  {
+    date: new Date('2023-04-08'),
+    position: 'Mid-level Full Stack Developer',
+    company: 'Lhoopa'
+  },
+  {
+    date: new Date('2024-07-15'),
+    position: 'Frontend Engineer',
+    company: 'Evelan'
+  },
+])
+</script>
+
+<template>
+  <Timeline :value="events">
+    <template #content="{ item }: {item: Event}">
+      <small>{{ item.date.toLocaleDateString() }}</small>
+      <p>{{ item.position }} @ {{ item.company }}</p>
+    </template>
+  </Timeline>
+</template>
---
+++ src/components/vue/Scroller.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  to: string
+}>()
+
+function scroll() {
+  document
+    .getElementById(props.to)
+    ?.scrollIntoView({ behavior: 'smooth' })
+}
+</script>
+
+<template>
+  <div class="flex transform animate-bounce cursor-pointer flex-col text-center transition-transform">
+    <i class="ri-arrow-down-double-line ri-4x" @click="scroll"></i>
+  </div>
+</template>
