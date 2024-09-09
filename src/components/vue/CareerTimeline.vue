bfe19c2d41fb44883b6508955aeb24db944e14d0
Jeric June Logan <loganjeric25@gmail.com>
2024-09-09 22:35:23 +0800

    finished timeline

--- src/components/vue/CareerTimeline.vue
+++ src/components/vue/CareerTimeline.vue
@@ -1,42 +1,51 @@
 <script setup lang="ts">
-import Timeline from "primevue/timeline"
-import { ref } from "vue";
+import type { CollectionEntry } from 'astro:content'
+import Timeline from 'primevue/timeline'
+import TechnologyTag from './TechnologyTag.vue'
 
-type Event = {
-  date: Date
-  position: string
-  company: string
-}
+type Career = CollectionEntry<'careers'>
+type Technology = CollectionEntry<'technologies'>
 
-const events = ref<Event[]>([
-  {
-    date: new Date('2022-08-23'),
-    position: 'Web Developer',
-    company: 'Prime Outsourcing'
-  },
-  {
-    date: new Date('2023-01-16'),
-    position: 'Junior Full Stack Developer',
-    company: 'Lhoopa'
-  },
-  {
-    date: new Date('2023-04-08'),
-    position: 'Mid-level Full Stack Developer',
-    company: 'Lhoopa'
-  },
-  {
-    date: new Date('2024-07-15'),
-    position: 'Frontend Engineer',
-    company: 'Evelan'
-  },
-])
+const props = defineProps<{
+    careers: Career[]
+    technologies: Technology[]
+}>()
+
+const getTechnology = (id: string) =>
+    props.technologies.find((x) => x.id === id) as Technology
 </script>
 
 <template>
-  <Timeline :value="events">
-    <template #content="{ item }: {item: Event}">
-      <small>{{ item.date.toLocaleDateString() }}</small>
-      <p>{{ item.position }} @ {{ item.company }}</p>
-    </template>
-  </Timeline>
+    <Timeline :value="careers" align="left" :pt="{ opposite: '!hidden' }">
+        <template #content="{ item: { data: item } }: { item: Career }">
+            <div class="mb-16 space-y-4 text-justify">
+                <small
+                    >{{
+                        item.date_from
+                            ? item.date_from.toLocaleDateString() + ' to'
+                            : ''
+                    }}
+                    {{ item.date_to?.toLocaleDateString() ?? 'Present' }}</small
+                >
+
+                <p class="text-left text-xl font-semibold">
+                    {{ item.position }} @ {{ item.company }}
+                </p>
+
+                <p>{{ item.description }}</p>
+
+                <div v-if="item.technologies">
+                    <p>I learned & used these technologies:</p>
+
+                    <div class="max-w-2/3 mt-4 flex flex-wrap gap-2">
+                        <TechnologyTag
+                            v-for="tech in item.technologies"
+                            :key="tech.id"
+                            :technology="getTechnology(tech.id)"
+                        />
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Timeline>
 </template>
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,46 +1,60 @@
 import { defineCollection, reference, z } from 'astro:content'
+import { zodToJsonSchema } from "zod-to-json-schema";
 
 const technologies = defineCollection({
-    type: 'data',
-    schema: z.object({
-        slug: z.string(),
-        name: z.string(),
-        type: z.array(z.string()),
-        logo: z.string(),
-        skill: z.number(),
-        interest: z.number(),
-        opinion: z.string(),
-    }),
+  type: 'data',
+  schema: z.object({
+    slug: z.string(),
+    name: z.string(),
+    type: z.array(z.string()),
+    logo: z.string(),
+    skill: z.number(),
+    interest: z.number(),
+    opinion: z.string(),
+  }),
 })
 
 const projects = defineCollection({
-    type: 'data',
-    schema: z.object({
-        name: z.string(),
-        type: z.enum(['personal', 'professional', 'freelance']),
-        repo_url: z.string().nullable(),
-        live_url: z.string().nullable(),
-        image: z.string().nullable(),
-        progress: z.number().nullable(),
-        description: z.string(),
-        technologies: reference('technologies').array().nullable(),
-    }),
+  type: 'data',
+  schema: z.object({
+    name: z.string(),
+    type: z.enum(['personal', 'professional', 'freelance']),
+    repo_url: z.string().nullable(),
+    live_url: z.string().nullable(),
+    image: z.string().nullable(),
+    progress: z.number().nullable(),
+    description: z.string(),
+    technologies: reference('technologies').array().nullable(),
+  }),
 })
 
 const posts = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        publishedDate: z.date().nullable(),
-        updatedDate: z.date().optional(),
-        image: z.string().optional(),
-        tags: z.array(z.string()).optional(),
-    }),
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedDate: z.date().nullable(),
+    updatedDate: z.date().optional(),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+})
+
+const careers = defineCollection({
+  type: "data",
+  schema: z.object({
+    date_from: z.string().transform((str) => new Date(str)).nullable(),
+    date_to: z.string().transform((str) => new Date(str)).nullable(),
+    position: z.string(),
+    company: z.string(),
+    description: z.string(),
+    technologies: reference('technologies').array().optional()
+  })
 })
 
 export const collections = {
-    technologies,
-    projects,
-    posts,
+  technologies,
+  projects,
+  posts,
+  careers,
 }
